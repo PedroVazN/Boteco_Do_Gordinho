eaf46eb71d02f2c7c1f044af977ff7538e1fbdf3
Pedro Vaz <vaznascimento23@gmail.com>
2025-08-18 21:43:37 -0300

    commit

---
+++ encomendas.js
@@ -0,0 +1,47 @@
+//document.querySelector(".total").textContent=document.querySelectir(".qtde").textContent = document.querySelector(".unitario").textContent
+//captura encomenda da tabela
+
+var clientes = document.querySelectorAll(".cliente")
+
+//Passa por todas as encomendas e calcula o valor total
+for (var count = 0; count < clientes.length; count++) {
+
+    //Quantidade de encomenda
+    var tab_qtde = clientes[count].querySelector(".qtde").textContent;
+
+    //valor do produto
+    var tab_unitario = clientes[count].querySelector(".unitario").textContent;
+
+    //Verifica se a quantidade é valida
+    if (tab_qtde < 1 || isNaN(tab_qtde)) {
+        // quantidade invalida
+        clientes[count].querySelector(".qtde").textContent = "Quantidade invalida";
+
+        //clientes[count].querySelector(".qtde").computedStyleMap.color = "red";
+        //Texto da quantidade em vermelho
+
+        clientes[count].style.backgroundColor = "red";
+    }
+
+    else if (tab_unitario < 1 || isNaN(tab_unitario)) {
+
+        clientes[count].querySelector(".unitario").textContent = "Valor Unitario";
+
+        clientes[count].querySelector(".unitario").style.color = "red";
+
+
+    } else {
+        clientes[count].querySelector(".total").textContent = calculaTotal
+        (tab_qtde, tab_unitario)
+    }
+
+}
+
+//Função de calculo do valor total
+function calculaTotal(qtde, unit) {
+    var total = 0;
+
+    total = qtde * unit;
+    
+    return total;
+}
