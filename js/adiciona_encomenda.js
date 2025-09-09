196ab4633e381b01b2b33d9520c6c8a441164d49
Pedro Vaz Nascimento <25170632@educ123.sp.senai.br>
2025-09-08 21:47:50 -0300

    Uopdate

--- js/adiciona_encomenda.js
+++ js/adiciona_encomenda.js
@@ -5,11 +5,20 @@ var table = document.querySelector("#tabela tbody")
 botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
 
+
+
     const nomeCliente = document.getElementById("nome").value
     const unit = document.getElementById("unit").value
     const qtde = document.getElementById("qtde").value
     const produto = document.getElementById("produtot").value
 
+    
+    if (!nomeCliente.value || !unit.value || !qtde.value || !produto.value) {
+        alert("Preencha todos os campos antes de adicionar.")
+        return
+    }
+
+    
     var novaTr = document.createElement("tr");
 
     novaTr.innerHTML = `
